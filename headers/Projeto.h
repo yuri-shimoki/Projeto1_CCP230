29644d68aeac3e8eddd9403bd81f145f6d1471ca
Yuri Shimoki <ysshimoki@protonmail.com>
2025-03-26 15:07:56 -0300

    Os arquivos .c e .h foram formatados seguindo um mesmo padrão.

--- headers/Projeto.h
+++ headers/Projeto.h
@@ -1,20 +1,22 @@
 #define TOTAL_TAREFAS 5
 
-typedef struct {
+typedef struct
+{
     int prioridade;
     char descricao[300];
     char categoria[100];
 } Tarefa;
 
-typedef struct {
+typedef struct
+{
     Tarefa tarefas[100];
     int qtd;
 } ListaDeTarefa;
 
-int criarTarefa(ListaDeTarefas *lt);
-int deletarTarefa(ListaDeTarefas *lt);
-int listarTarefas(ListaDeTarefas *lt);
+int criarTarefa(ListaDeTarefas* lt);
+int deletarTarefa(ListaDeTarefas* lt);
+int listarTarefas(ListaDeTarefas* lt);
 
-int carregarTarefas(ListaDeTarefas *lt, char *nomes);
-int salvarTarefas(ListaDeTarefas *lt, char *nome);
+int carregarTarefas(ListaDeTarefas* lt, char* nomes);
+int salvarTarefas(ListaDeTarefas* lt, char* nome);
 void exibeMenu();
--- src/main.c
+++ src/main.c
@@ -1,51 +1,59 @@
-#include "projeto.h"
 #include "stdio.h"
+#include "projeto.h"
 
-int main(){
-ListaDeTarefas lt;
+int main(void)
+{
+    ListaDeTarefas lt;
 
-char arquivo[] = "tarefas";
-int codigo, opcao;
+    char arquivo[] = "tarefas";
+    int codigo, opcao;
 
-codigo=carregarTarefas(lt, arquivo);
+    codigo = carregarTarefas(lt, arquivo);
 
-if (codigo !=0){
-printf("Lista de tarefas nao carregada");
-lt.qtd=2;
-}
-
-do{
-exibeMenu();
-scanf("%d", &opcao);
-
-if(opcao == 0){}
-else if(opcao == 1){
-    codigo=criarTarefa(&lt);
-    if (codigo ==1 )
-    printf("erro ao criar tarefa: Sem espacao disponivel\n");
+    if (codigo !=0)
+    {
+        printf("Lista de tarefas nao carregada");
+        lt.qtd = 2;
     }
-else if(opcao == 2){
-    codigo=deletarTarefa(&lt);
-    if (codigo ==1 )
-    printf("erro ao deletar tarefa: nao existem tarefas para deletar\n");
-else if(codigo ==2)
-    printf("erro ao deletar tarefa: posicao invalida\n");
-}
-else if(opcao == 3){
-    codigo=listarTarefas(&lt);
-   if (codigo ==2)
-   printf("Erro ao listar tarefas: nao existem tarefas para serem listadas");
-}
-else {
-	printf("opcao invalida\n");
-}
-}while (opcao != 0);
 
-codigo=salvarTarefas(&lt,arquivo);
-if(codigo ==0)
-printf("Erro ao salvar tarefas em arquivo");
+    do
+    {
+        exibeMenu();
+        scanf("%d", &opcao);
 
+        if(opcao == 0)
+        {
 
-system ("pause");
+        }
+        else if(opcao == 1)
+        {
+            codigo = criarTarefa(&lt);
+            if (codigo == 1)
+            printf("erro ao criar tarefa: Sem espacao disponivel\n");
+        }
+        else if(opcao == 2)
+        {
+            codigo=deletarTarefa(&lt);
 
+            if (codigo == 1)
+                printf("erro ao deletar tarefa: nao existem tarefas para deletar\n");
+            else if(codigo == 2)
+                printf("erro ao deletar tarefa: posicao invalida\n");
+        }
+        else if(opcao == 3)
+        {
+            codigo=listarTarefas(&lt);
+            if (codigo ==2)
+            printf("Erro ao listar tarefas: nao existem tarefas para serem listadas");
+        }
+        else
+            printf("opcao invalida\n");
+
+    } while (opcao != 0);
+
+    codigo = salvarTarefas(&lt, arquivo);
+    if (codigo == 0)
+        printf("Erro ao salvar tarefas em arquivo");
+
+    system("pause");
 }
