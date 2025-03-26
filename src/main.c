615f9517d0cd1084b305ac0e3b8db9abea3e1c06
Yuri Shimoki <ysshimoki@protonmail.com>
2025-03-26 15:24:02 -0300

    Código foi formatado seguindo um padrão.

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(void)
 
     codigo = carregarTarefas(&lt, arquivo);
 
-    if (codigo !=0)
+    if (codigo != 0)
     {
         printf("Lista de tarefas nao carregada");
         lt.qtd = 2;
@@ -22,29 +22,29 @@ int main(void)
         exibeMenu();
         scanf("%d", &opcao);
 
-        if(opcao == 0)
+        if (opcao == 0)
         {
 
         }
-        else if(opcao == 1)
+        else if (opcao == 1)
         {
             codigo = criarTarefa(&lt);
             if (codigo == 1)
             printf("erro ao criar tarefa: Sem espacao disponivel\n");
         }
-        else if(opcao == 2)
+        else if (opcao == 2)
         {
             codigo=deletarTarefa(&lt);
 
             if (codigo == 1)
                 printf("erro ao deletar tarefa: nao existem tarefas para deletar\n");
-            else if(codigo == 2)
+            else if (codigo == 2)
                 printf("erro ao deletar tarefa: posicao invalida\n");
         }
         else if(opcao == 3)
         {
-            codigo=listarTarefas(&lt);
-            if (codigo ==2)
+            codigo = listarTarefas(&lt);
+            if (codigo == 2)
             printf("Erro ao listar tarefas: nao existem tarefas para serem listadas");
         }
         else
